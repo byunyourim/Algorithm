0c531de0e10aeb77f80a073d22a9e1cacf8db089
winnie <byourim@gmail.com>
2024-10-27 21:11:33 +0900

    Create Solution.java

---
+++ Programmers/week6/11. 문자열 나누기/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(String s) {
+    int answer = 0;
+    int index = 0;
+    while (index < s.length()) {
+        char x = s.charAt(index);
+        int xCount = 0;
+        int otherCount = 0;
+
+        while (index < s.length()) {
+            if (s.charAt(index) == x) {
+                xCount ++;
+            } else {
+                otherCount++;
+            }
+            index++;
+
+            if (xCount == otherCount) {
+                answer++;
+                break;
+            }
+        }
+
+
+       if (index == s.length() && xCount != otherCount) {
+            answer++;
+        }
+
+    }
+
+    return answer;
+    }
+}
