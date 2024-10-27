69a2ecf0e3a244609a8c39cbecf70f1aaf7d8b06
winnie <byourim@gmail.com>
2024-10-27 13:56:27 +0900

    Create Solution.java

---
+++ Programmers/week6/08. 푸드 파이트 대회/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            int n = 0;
+            if (food[i] % 2 == 0  ) {
+                n = food[i] / 2;
+            } else if (food[i] % 2 != 0 & food[i] > 1) {
+                n = (food[i] - 1) / 2;
+            } else if (food[i] == 1) continue;
+            
+            int index = 0;
+            
+            while (index < n) {
+                result.append(i);
+                index++;
+            }
+        }
+        String reverseStr = new StringBuilder(result).reverse().toString();
+        result.append(0).append(reverseStr);
+        
+        return result.toString();
+    }
+}
