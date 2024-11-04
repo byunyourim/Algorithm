51b7005825f9a4225ed68674b78b81463e6a70ff
Yu Rim Byun <byourim@gmail.com>
2024-11-04 12:38:20 +0900

    Create Solution2.java

---
+++ Programmers/week8/02. 2개 이하로 다른 비트/Solution2.java
@@ -0,0 +1,11 @@
+// https://hstory0208.tistory.com/entry/Java%EC%9E%90%EB%B0%94-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Lv2-2%EA%B0%9C-%EC%9D%B4%ED%95%98%EB%A1%9C-%EB%8B%A4%EB%A5%B8-%EB%B9%84%ED%8A%B8
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = numbers.clone();
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i]++;
+            answer[i] += (answer[i] ^ numbers[i]) >> 2;
+        }
+        return answer;
+    }
+}
