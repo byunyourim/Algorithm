f0154bc6ac0c053f56db54f7b975bb24c148d16e
Yu Rim Byun <byourim@gmail.com>
2024-11-04 10:50:42 +0900

    Update Solution2.java

--- Programmers/week8/01. 파일명 정렬/Solution2.java
+++ Programmers/week8/01. 파일명 정렬/Solution2.java
@@ -2,34 +2,79 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] files) {
-        List<Map<String, Integer>> dataList = new ArrayList<>();
+        List<FileInfo> fileList = new ArrayList<>();
+        
         for (String file : files) {
-            String numStr = extractLeadingNumber(file);
-            dataList.add(Map.of(file, Integer.parseInt(numStr)));
+            FileInfo fileInfo = parseFile(file);
+            fileList.add(fileInfo);
         }
         
-        // HEAD와 NUMBER로 정렬
-        dataList.sort(Comparator.comparing((Map<String, Integer> map) -> {
-                    String key = map.keySet().iterator().next();
-                    String head = key.replaceAll("[0-9].*", ""); // 숫자 이후의 모든 부분을 제거
-                    return head.toLowerCase(); // 대소문자 구분 없이 정렬
-                })
-                .thenComparing(map -> map.values().iterator().next())); // NUMBER로 정렬
-
+        // 정렬
+        Collections.sort(fileList);
+        
+        // 정렬된 결과를 배열로 변환
         String[] result = new String[files.length];
-        int idx = 0;
-        for (Map<String, Integer> data : dataList) {
-            result[idx] = data.keySet().iterator().next(); // 원래 파일 이름 반환
-            idx++;
+        for (int i = 0; i < fileList.size(); i++) {
+            result[i] = fileList.get(i).originalName;
         }
         
         return result;
     }
     
-    public static String extractLeadingNumber(String text) {
-        // 숫자를 추출하는 메서드
-        String result = text.replaceAll("^[^0-9]*", ""); // 숫자 앞의 문자 제거
-        result = result.replaceAll("([0-9]+).*", "$1"); // 첫 번째 숫자 부분만 추출
-        return result;
+    // 파일 정보를 담는 내부 클래스
+    private static class FileInfo implements Comparable<FileInfo> {
+        String originalName;
+        String head;
+        int number;
+        String tail;
+        
+        FileInfo(String originalName, String head, int number, String tail) {
+            this.originalName = originalName;
+            this.head = head.toLowerCase(); // 대소문자 구분 없이 처리
+            this.number = number;
+            this.tail = tail;
+        }
+        
+        @Override
+        public int compareTo(FileInfo other) {
+            // HEAD 비교
+            int headComparison = this.head.compareTo(other.head);
+            if (headComparison != 0) {
+                return headComparison; // HEAD가 다르면 그 결과 반환
+            }
+            // NUMBER 비교
+            return Integer.compare(this.number, other.number); // NUMBER는 정수로 비교
+        }
+    }
+    
+    // 파일명을 파싱하여 FileInfo 객체 생성
+    private FileInfo parseFile(String file) {
+        String head = "";
+        String numberStr = "";
+        String tail = "";
+
+        // HEAD 찾기
+        int i = 0;
+        while (i < file.length() && !Character.isDigit(file.charAt(i))) {
+            head += file.charAt(i);
+            i++;
+        }
+        
+        // NUMBER 찾기
+        while (i < file.length() && Character.isDigit(file.charAt(i))) {
+            numberStr += file.charAt(i);
+            i++;
+        }
+        
+        // TAIL 찾기
+        while (i < file.length()) {
+            tail += file.charAt(i);
+            i++;
+        }
+
+        // NUMBER를 정수로 변환
+        int number = Integer.parseInt(numberStr);
+        
+        return new FileInfo(file, head, number, tail);
     }
 }
