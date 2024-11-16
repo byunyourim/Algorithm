0d82aa588a20298c515443c3ccca80ec21117ae5
Yu Rim Byun <byourim@gmail.com>
2024-11-16 13:34:24 +0900

    11. 큰 수 만들기 Solution2.java

---
+++ Programmers/week9/11. 큰 수 만들기/Solution2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder builder = new StringBuilder();
+        int remaining = number.length() - k;
+        int idx = 0;
+        
+        for (int i = 0; i < remaining; i++) {
+            char max = 0;
+            
+            for (int j = idx; j <= i + k; j++) {
+                if (max < number.charAt(j)) {
+                    max = number.charAt(j);
+                    idx = j + 1;
+                }
+            }
+            
+            builder.append(max);
+        }
+        return builder.toString();
+    }
+}
