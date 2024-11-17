ff850ff940e033bf8b0ccc6a14408aadf460853b
Yu Rim Byun <byourim@gmail.com>
2024-11-17 16:06:40 +0900

    Create Solution.java

---
+++ Programmers/week9/14. 두 원 사이의 정수 쌍/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {a
+    public long solution(int r1, int r2) {
+        long count = 0;
+        
+        for (int x = 1; x <= r2; x++) {
+            long maxY = (long)Math.floor(Math.sqrt(1.0 * r2 * r2 - 1.0 * x * x));
+            long minY = (long)Math.ceil(Math.sqrt(1.0 * r1 * r1 - 1.0 * x * x));
+
+            count += (maxY - minY + 1);
+        }
+        
+        return count * 4;
+    }
+}
