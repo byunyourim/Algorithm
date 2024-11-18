d1654a502affd9a3e7233c9fe96c4f29ea97214d
Yu Rim Byun <byourim@gmail.com>
2024-11-18 11:11:05 +0900

    01. 점 찍기 - 문제 풀이 완료
    소스 코드 추가

--- Programmers/week10/01. 점 찍기/Solution.java
+++ Programmers/week10/01. 점 찍기/Solution.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Solution {
-    public long solution(int k, int d) {
+    public long solution(int k, int d) { 
         long cnt = 0;
         for (long x = 0; x <= d; x+=k) {
             long maxY = (long) Math.sqrt((long) d * d - (long) x * x);
