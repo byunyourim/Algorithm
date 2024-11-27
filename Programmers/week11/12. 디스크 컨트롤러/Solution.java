dcdcfd2365305571e92d375341a2d928ef74644f
Yu Rim Byun <byourim@gmail.com>
2024-11-27 12:47:51 +0900

    12. 디스크 컨트롤러 - 문제 풀이 1

---
+++ Programmers/week11/12. 디스크 컨트롤러/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        int end = 0;
+        int jobsIndex = 0;
+        int count = 0;
+        
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        
+        while (count < jobs.length) {
+            while (jobsIndex < jobs.length && jobs[jobsIndex][0] <= end) {
+                pq.add(jobs[jobsIndex++]);
+            }
+            
+            if (pq.isEmpty()) {
+                end = jobs[jobsIndex][0];
+            } else {
+                int[] temp = pq.poll();
+                answer += temp[1] + end - temp[0];
+                end += temp[1];
+                count++;
+            }
+        }
+        
+        return (int) Math.floor(answer / jobs.length);
+    }
+}
